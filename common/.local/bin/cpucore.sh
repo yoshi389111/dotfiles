ef41bc5a8294590a33e9c4a1fcee56230514ca95
sato yoshiyuki <yoshi.389111@gmail.com>
2025-03-17 20:00:49 +0900

    fix: cpucore.sh

--- common/.local/bin/cpucore.sh
+++ common/.local/bin/cpucore.sh
@@ -5,8 +5,8 @@ CORES=$( fgrep 'cpu cores' /proc/cpuinfo | sort -u | sed 's/.*: //' )
 L_PRC=$( fgrep 'processor' /proc/cpuinfo | wc -l )
 H_TRD=$(( L_PRC /  P_CPU / CORES ))
 
-echo -n "${L_PRC} processer" ; [ "${L_PRC}" -ne 1 ] && echo -n "s"
-echo -n " = ${P_CPU} socket" ; [ "${P_CPU}" -ne 1 ] && echo -n "s"
-echo -n " x ${CORES} core"   ; [ "${CORES}" -ne 1 ] && echo -n "s"
-echo -n " x ${H_TRD} thread" ; [ "${H_TRD}" -ne 1 ] && echo -n "s"
-echo
+printf "${L_PRC} processer" ; [ "${L_PRC}" -ne 1 ] && printf "s"
+printf " = ${P_CPU} socket" ; [ "${P_CPU}" -ne 1 ] && printf "s"
+printf " x ${CORES} core"   ; [ "${CORES}" -ne 1 ] && printf "s"
+printf " x ${H_TRD} thread" ; [ "${H_TRD}" -ne 1 ] && printf "s"
+printf "\n"
