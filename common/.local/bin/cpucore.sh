5cdeba661ba0a378965b8ff7e972d2f79bc00cf2
sato yoshiyuki <yoshi.389111@gmail.com>
2025-08-05 07:17:58 +0900

    refactor: improve cpu information retrieval and output formatting in cpucore.sh

--- common/.local/bin/cpucore.sh
+++ common/.local/bin/cpucore.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
 
-P_CPU=$( fgrep 'physical id' /proc/cpuinfo | sort -u | wc -l )
-CORES=$( fgrep 'cpu cores' /proc/cpuinfo | sort -u | sed 's/.*: //' )
-L_PRC=$( fgrep 'processor' /proc/cpuinfo | wc -l )
+P_CPU=$( grep -F 'physical id' /proc/cpuinfo | sort -u | wc -l )
+CORES=$( grep -F 'cpu cores' /proc/cpuinfo | sort -u | sed 's/.*: //' )
+L_PRC=$( grep -Fc 'processor' /proc/cpuinfo )
+
+if [ "$P_CPU" -eq 0 ] || [ "$CORES" -eq 0 ]; then
+  echo "Error: /proc/cpuinfo から情報を取得できませんでした。" >&2
+  exit 1
+fi
+
 H_TRD=$(( L_PRC /  P_CPU / CORES ))
 
-printf "${L_PRC} processer" ; [ "${L_PRC}" -ne 1 ] && printf "s"
-printf " = ${P_CPU} socket" ; [ "${P_CPU}" -ne 1 ] && printf "s"
-printf " x ${CORES} core"   ; [ "${CORES}" -ne 1 ] && printf "s"
-printf " x ${H_TRD} thread" ; [ "${H_TRD}" -ne 1 ] && printf "s"
+## usage: print_num_with_units NUM UNIT UNITS
+print_num_with_units() {
+  if [ "$1" -eq 1 ]; then
+    printf "%s %s" "$1" "$2"
+  else
+    printf "%s %s" "$1" "$3"
+  fi
+}
+
+print_num_with_units "$L_PRC" "processor" "processors"
+printf " = "
+print_num_with_units "$P_CPU" "socket" "sockets"
+printf " x "
+print_num_with_units "$CORES" "core" "cores"
+printf " x "
+print_num_with_units "$H_TRD" "thread" "threads"
 printf "\n"
