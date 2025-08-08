a6a7532ba9e2d55846f65a64569307ce0bfbf0b8
sato yoshiyuki <yoshi.389111@gmail.com>
2025-08-08 19:27:17 +0900

    refact: improve code formatting and consistency in multiple scripts

--- common/.local/bin/errno.sh
+++ common/.local/bin/errno.sh
@@ -1,20 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [[ $# -ne 1 || "$1" = "-h" || "$1" = "--help" ]]; then
   echo "Usage: ${0##*/} { ERROR_NUMBER | ERROR_NAME }" >&2
   exit 1
 
-elif [[ ! ( "$1" =~ ^E[A-Z0-9]+$ || "$1" =~ ^[1-9][0-9]*$ ) ]]
-then
+elif [[ ! ("$1" =~ ^E[A-Z0-9]+$ || "$1" =~ ^[1-9][0-9]*$) ]]; then
   echo "illegal argument." >&2
   exit 1
 
-elif ! find /usr/include -name 'errno*.h' -print0 |
-  xargs -0 -r grep -Eh \
-    -e "^\\s*#\\s*define\\s+E[A-Z0-9]+\\s+$1\\>" \
-    -e "^\\s*#\\s*define\\s+$1\\s+[1-9][0-9]*\\>" \
-    -e "^\\s*#\\s*define\\s+$1\\s+E[A-Z0-9]+\\>"
-then
+elif ! find /usr/include -name 'errno*.h' \
+  -exec grep -Eh \
+  -e "^\\s*#\\s*define\\s+E[A-Z0-9]+\\s+$1\\>" \
+  -e "^\\s*#\\s*define\\s+$1\\s+[1-9][0-9]*\\>" \
+  -e "^\\s*#\\s*define\\s+$1\\s+E[A-Z0-9]+\\>" \
+  '{}' '+'; then
   echo "errno not found($1)." >&2
   exit 1
 fi
--- common/.local/bin/web.sh
+++ common/.local/bin/web.sh
@@ -3,12 +3,12 @@
 
 port=8000
 
-if type python3 > /dev/null 2>&1; then
-    python3 -m http.server $port
-elif type ruby > /dev/null 2>&1; then
-    ruby -run -e httpd . -p $port
-elif type busybox > /dev/null 2>&1; then
-    busybox httpd -f -p $port
+if type python3 >/dev/null 2>&1; then
+  python3 -m http.server $port
+elif type ruby >/dev/null 2>&1; then
+  ruby -run -e httpd . -p $port
+elif type busybox >/dev/null 2>&1; then
+  busybox httpd -f -p $port
 else
-    echo "No web server found"
+  echo "No web server found"
 fi
